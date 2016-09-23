e9773d60b1c6f5ae1d12121b55eb32e320168128
xpg <18720918557@163.com>
2016-09-23 11:41:18 +0800

    创建ChartView

---
+++ app/src/main/java/com/example/nanfu/libs/view/ChartView.java
@@ -0,0 +1,23 @@
+package com.example.nanfu.libs.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2016/9/23.
+ */
+
+public class ChartView extends View {
+    public ChartView(Context context) {
+        super(context);
+    }
+
+    public ChartView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ChartView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
